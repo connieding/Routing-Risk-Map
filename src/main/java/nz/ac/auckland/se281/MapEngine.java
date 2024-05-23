3a7c7eedfae4e6feebff9df7f8f2ff5d019a1230
Connie Ding <cdin152@aucklanduni.ac.nz>
2024-05-23 21:15:14 +1200

    Implemented the same code and reused some of task 1 code to ask user for the source and destination country in showRoute method

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -7,6 +7,8 @@ import java.util.Set;
 /** This class is the main entry point. */
 public class MapEngine {
   Set<Country> countryInfo = new HashSet<>();
+  Country beginCountry;
+  Country finalCountry;
 
   public MapEngine() {
     // add other code here if you want
@@ -33,10 +35,10 @@ public class MapEngine {
   /** this method is invoked when the user run the command info-country. */
   public void showInfoCountry() {
     // add code here
+    // Ask the user to enter the country name
+    MessageCli.INSERT_COUNTRY.printMessage();
 
     while (true) {
-      // Ask the user to enter the country name
-      MessageCli.INSERT_COUNTRY.printMessage();
       String countryInput = Utils.scanner.nextLine();
       countryInput = Utils.capitalizeFirstLetterOfEachWord(countryInput);
 
@@ -56,15 +58,39 @@ public class MapEngine {
 
   /** this method is invoked when the user run the command route. */
   public void showRoute() {
+
     MessageCli.INSERT_SOURCE.printMessage();
-    String startCountry = Utils.scanner.nextLine();
+    while (true) {
+      // Ask the user to enter the country name
+      String startCountry = Utils.scanner.nextLine();
+      startCountry = Utils.capitalizeFirstLetterOfEachWord(startCountry);
+
+      // Print the country information
+      try {
+        beginCountry = checkCountryName(startCountry);
+        break;
+      } catch (CountryNotFoundException e) {
+        System.out.println(e.getMessage());
+      }
+    }
 
     MessageCli.INSERT_DESTINATION.printMessage();
-    String endCountry = Utils.scanner.nextLine();
+    while (true) {
+      // Ask the user to enter the country name
+      String endCountry = Utils.scanner.nextLine();
+      endCountry = Utils.capitalizeFirstLetterOfEachWord(endCountry);
+
+      try {
+        finalCountry = checkCountryName(endCountry);
+        break;
+      } catch (CountryNotFoundException e) {
+        System.out.println(e.getMessage());
+      }
+    }
   }
 
   public Country checkCountryName(String countryInput) throws CountryNotFoundException {
-    // Print out the country information
+
     for (Country country : countryInfo) {
       if (country.getCountryName().equals(countryInput)) {
         return country;
