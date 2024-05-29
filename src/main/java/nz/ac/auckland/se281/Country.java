0cfb5144fd691ae442e44b01bff82121a245bbd9
Connie Ding <cdin152@aucklanduni.ac.nz>
2024-05-30 01:02:14 +1200

    Added missing Javadoc and added more comments in Country class

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -6,12 +6,24 @@ public class Country {
   private String continent;
   private int taxFees;
 
+  /**
+   * Constructor to create a country object.
+   *
+   * @param countryName the name of the country
+   * @param continent the continent of the country
+   * @param taxFees the tax fees of the country
+   */
   public Country(String countryName, String continent, int taxFees) {
     this.countryName = countryName;
     this.continent = continent;
     this.taxFees = taxFees;
   }
 
+  /**
+   * Get the string representation of the country.
+   *
+   * @return the string representation of the country
+   */
   @Override
   public int hashCode() {
     final int prime = 31;
@@ -20,6 +32,12 @@ public class Country {
     return result;
   }
 
+  /**
+   * Check if the country is equal to another object.
+   *
+   * @param obj the object to compare
+   * @return true if the country is equal to the object, false otherwise
+   */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
@@ -42,14 +60,29 @@ public class Country {
     return true;
   }
 
+  /**
+   * Get the name of the country.
+   *
+   * @return the name of the country
+   */
   public String getCountryName() {
     return countryName;
   }
 
+  /**
+   * Get the continent of the country.
+   *
+   * @return the continent of the country
+   */
   public String getContinent() {
     return continent;
   }
 
+  /**
+   * Get the tax fees of the country.
+   *
+   * @return the tax fees of the country
+   */
   public int getTaxFees() {
     return taxFees;
   }
